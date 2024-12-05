59a03eda7d96068a593152ecb0d7bce90e318927
CuteEvilMan <qwewewasd@outlook.com>
2024-12-05 09:00:14 +0800

    Update huffman6_NB_suvvess.c

--- huffman6_NB_suvvess.c
+++ huffman6_NB_suvvess.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #define CHAR 256
 #define end_mark "\n|\n|\n"
-const long Huffman_max = 102400L;
+const long Huffman_max = 1024L*128L;
 // 定义霍夫曼树的节点结构体
 typedef struct node {
     char character;       // 节点存储的字符
@@ -171,7 +171,7 @@ int isLeafNode(const Node* node) {
 // 打印霍夫曼编码
 void printCodes(const Node* root, int arr[], int top, FILE *file,HuffmanCode* huffman_code) {
 
-    if (top >= 100 || root==NULL||huffman_code==NULL) {
+    if (top >= 99 || root==NULL||huffman_code==NULL) {
         return;
     }
     if (root->leftChild) {
@@ -183,7 +183,7 @@ void printCodes(const Node* root, int arr[], int top, FILE *file,HuffmanCode* hu
         printCodes(root->rightChild, arr, top + 1, file,huffman_code);
     }
     if (isLeafNode(root)) {
-       fprintf(file, "%15x|%15u\n", root->character,root->frequency);
+       fprintf(file, "%15d|%15X\n", root->character,root->frequency);
         if (top==0) {
             huffman_code[root->character+CHAR/2].huffmanCode[0]=0;
             huffman_code[root->character+CHAR/2].huffmanCode[100]=1;
@@ -197,11 +197,15 @@ void printCodes(const Node* root, int arr[], int top, FILE *file,HuffmanCode* hu
 }
  FILE* huffman_encode_r(const char*in,const char*out_main,const char*out_help)
 {
-    FILE *fp0 = fopen(in, "r+b");
-    FILE *fp1 = fopen(out_main, "w+b");
-    FILE *fp2 = fopen(out_help, "w+b");
+    if (Huffman_max<1024) {
+        perror("压缩量太小");
+        return NULL;
+    }//当编码最大值国小时，拒绝编码
+    FILE *fp0 = fopen(in, "r+b");/*待压缩文件*/
+    FILE *fp1 = fopen(out_main, "w+b");/*输出压缩后的文件*/
+    FILE *fp2 = fopen(out_help, "w+b");/*输出压缩的字节及其频率*/
     if (fp0==NULL||fp1==NULL||fp2==NULL)
-       {
+       {/*检测是否有文件打开失败*/
         fclose(fp0);
         fclose(fp1);
         fclose(fp2);
@@ -219,7 +223,7 @@ void printCodes(const Node* root, int arr[], int top, FILE *file,HuffmanCode* hu
         ListNode *huffmanList = createListNode(createNode(inputChar, 1));//开始创建Huffman树
         long total=1L;//记录输入字符总数，判断是否需要重新开始
        int number=1;//记录输入字符种类，输入到辅助解码文件
-        while (total != Huffman_max)
+        while (total < Huffman_max)
             {/*当总数超过规定数量或读到文件尾时停止*/
            if ( fread(&inputChar, sizeof(char), 1, fp0) != 1 || feof(fp0))
                break;
@@ -228,37 +232,47 @@ void printCodes(const Node* root, int arr[], int top, FILE *file,HuffmanCode* hu
         }
         buildHuffmanTree(&huffmanList);//将Huffman树构建出来
         HuffmanCode* huffman_code=(HuffmanCode*)malloc(CHAR*sizeof(HuffmanCode));
-        for (int i = 0; i < CHAR; ++i) {/*存储Huffman编码并将频率输出到辅助解码文件，帮助解码*/
+        /*存储Huffman编码并将频率输出到辅助解码文件，帮助解码*/
+        for (int i = 0; i < CHAR; ++i) {
             huffman_code[i].huffmanCode[100] = 0;
-        }
+        }/*将记录Huffman编码位数的一位设置为零*/
         fprintf(fp2, end_mark);
-        fprintf(fp2, "%d\n", number);
+        fprintf(fp2, "%d\n", number);/*输出记录被编码的字符种类*/
         int codeArray[100], top = 0;
         printCodes(huffmanList->nodeData, codeArray, top, fp2,huffman_code);
+        //保存Huffman编码
         fseek(fp0, -total, SEEK_CUR);
+        /*返回开始进行Huffman编码的位置，开始编码*/
         total=0L;
-        while ( total != Huffman_max) {
+        /*进行编码的程序*/
+        while ( total < Huffman_max) {
             if (fread(&inputChar, sizeof(char), 1,fp0) == 0||feof(fp0))
             {break;}
-               total++;
+               total++;/*当已经编码的字符达到要求最大值或到达文件尾时，终止编码*/
             for(int i=0;i<huffman_code[inputChar+CHAR/2].huffmanCode[100];++i) {
                 if(huffman_code[inputChar+CHAR/2].huffmanCode[i]==0 )
-                    { cunchu &= ~(1 << count);}else{cunchu |= 1 << count;}
-                count++;if(count==8) {
+                    { cunchu &= ~(1 << count);}
+                else
+                    {cunchu |= 1 << count;}
+                count++;/*通过位操作将Huffman编码存入cunchu中
+                每存满8位，将该字节输出*/
+                if(count>=8) {
                     fwrite(&cunchu, sizeof(unsigned char), 1, fp1);
                     count = 0;cunchu=0b11111111;
                 }
             }
         }
+        /*当一次编码结束后，cunchu和count会被存储，等待下次编码时再次输入字符或编码结束时进行处理*/
         freeHuffmanList(huffmanList);
         huffmanList=NULL;
         free(huffman_code);
         huffman_code=NULL;
+        /*释放Huffman树和Huffman编码占用的内存，再次开始*/
     }
    fprintf(fp2, "" end_mark "%d  ", count)==0;
     for (int i=0;i<count ;i++) {
         fprintf(fp2, "%d ",cunchu & 1<<i ? 1 : 0);
-    }
+    }/*将最后剩余的编码输出到文件中，方便解码*/
     fclose(fp0);
     fclose(fp1);
     fclose(fp2);
@@ -266,9 +280,14 @@ void printCodes(const Node* root, int arr[], int top, FILE *file,HuffmanCode* hu
 }
 
  FILE* huffman_decode_r(const char*in0,const char*in1,const char*out) {
+
+    if (Huffman_max<1024) {
+        perror("解码大小太小了");
+        exit(EXIT_FAILURE);
+    }
     FILE* In0= fopen(in0, "r+b");/*编码后文件存储的位置*/
     FILE*In1= fopen(in1, "r+b");/*存储字节对应频率的文件*/
-    FILE* Out= fopen(out, "w+b");
+    FILE* Out= fopen(out, "w+b");/*待解码文件*/
     if (In0 ==NULL || In1==NULL || Out == NULL) {
         fclose(In0);
         fclose(In1);
@@ -280,13 +299,14 @@ void printCodes(const Node* root, int arr[], int top, FILE *file,HuffmanCode* hu
     long total=0L;//用于记录解码字节的数量
     ListNode *huffmanList = NULL;
     Node *root=NULL;//huffman树树根
-    Node *currentNode;;
+    Node *currentNode;//编码过程中
+    /*先进行一次读取获得所需Huffman编码*/
     int into=0;
     fscanf(In1, end_mark "%d", &into);
     for (int i = 0; i < into; ++i){
         int c;
         unsigned int d;
-        fscanf(In1, "%15x|%15u|\n", &c, &d);
+        fscanf(In1, "%15d|%15X|\n", &c, &d);
         Node *newNode = createNode((char)c, d);
         if (huffmanList == NULL) {
             huffmanList = createListNode(newNode);
@@ -297,10 +317,11 @@ void printCodes(const Node* root, int arr[], int top, FILE *file,HuffmanCode* hu
             }
             current->nextNode = createListNode(newNode);
         }
-    }
+    }/*从辅助文件中获得Huffman树*/
     buildHuffmanTree(&huffmanList);
     root = huffmanList->nodeData;
     currentNode = root;
+    /*依次读取压缩后的字符，进行压缩*/
       while(fread(&bit,sizeof(unsigned char),1,In0) == 1 && !feof(In0))
       {
             for(int i=0;i<8 && currentNode!=NULL;++i) {
@@ -308,12 +329,14 @@ void printCodes(const Node* root, int arr[], int top, FILE *file,HuffmanCode* hu
                     currentNode = currentNode->rightChild;
                 } else
                     currentNode = currentNode->leftChild;
-
-                if( isLeafNode(currentNode)) {
-                    fwrite(&currentNode->character, sizeof(char), 1, Out);
-                    currentNode = root;
+                //移动一个指针进行解码
+                if( isLeafNode(currentNode)) {//当指针移动到一个叶节点时
+                    fwrite(&currentNode->character, sizeof(char), 1, Out);//完成解码
+                    currentNode = root;//指针回归根节点，再次开始解码
+                    /*记录已解码出的字节的total+1*/
                     total++;
-                    if (total == Huffman_max || currentNode==NULL ) {
+                    /*当解码字节数达标时，重构Huffman树*/
+                    if (total >= Huffman_max || currentNode==NULL ) {
                         total=0L;
                         freeHuffmanList(huffmanList);
                         huffmanList = NULL;
@@ -321,7 +344,7 @@ void printCodes(const Node* root, int arr[], int top, FILE *file,HuffmanCode* hu
                         for (int ij = 0; ij < into; ++ij){
                             int c;
                             unsigned int d;
-                            fscanf(In1, "%15x|%15u|\n", &c, &d);
+                            fscanf(In1, "%15d|%15X|\n", &c, &d);
                             Node *newNode = createNode((char)c, d);
                             if (huffmanList == NULL) {
                                 huffmanList = createListNode(newNode);
@@ -340,10 +363,9 @@ void printCodes(const Node* root, int arr[], int top, FILE *file,HuffmanCode* hu
                 }
             }
     }
-
+/*对编码过程中剩余不足一个字节的内容的进行解码*/
     int lin;
     fscanf(In1, end_mark "%d " , &lin);
-   // int into;
     for(int i=0;i<lin && currentNode!=NULL;++i)
     {
         fscanf(In1, "%d" , &into);
@@ -359,14 +381,14 @@ void printCodes(const Node* root, int arr[], int top, FILE *file,HuffmanCode* hu
     fclose(In0);
     fclose(In1);
     fclose(Out);
-
     return Out;
-
 }
 #endif //HUFFMAN_H
 int main() {
-    huffman_encode_r("D:\\1.txt","D:\\test.txt","D:\\0.txt");
-    huffman_decode_r("D:\\test.txt","D:\\0.txt","D:\\0hjk.txt");
-
+    if (Huffman_max<=1024) {
+        perror("wrong input");
+        exit(EXIT_FAILURE);
+    }
+    huffman_encode_r("D:\\1.exe","D:\\test.txt","D:\\0.txt");
+    huffman_decode_r("D:\\test.txt","D:\\0.txt","D:\\0hjk.exe");
 }
-
